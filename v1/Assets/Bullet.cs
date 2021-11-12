08bc9bc492beaef745ffd4ebcd6ad85d6a7955f1
Titoo <techin.spp@gmail.com>
2021-11-13 05:19:48 +0700

    first POC fire

--- v1/Assets/Bullet.cs
+++ v1/Assets/Bullet.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed = 8f;
+    public float speed = 16f;
+    private Vector3 firstPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        firstPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += transform.forward * speed * Time.deltaTime;
+        if (Vector3.Distance(transform.position, firstPosition) > 100) {
+            Destroy(this.gameObject);
+        }
     }
 }
---
+++ v1/Assets/Weapon.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bullet;
+    public Rigidbody boxRigid;
+    private double firingRate = 0.25;
+    private double remainingFiringRate = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) {
+            boxRigid.AddForce(new Vector3(-1f, 0f, 0f));
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            boxRigid.AddForce(new Vector3(1f, 0f, 0f));
+        }
+        if (Input.GetKey(KeyCode.W)) {
+            boxRigid.AddForce(new Vector3(0f, 0f, 1f));
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            boxRigid.AddForce(new Vector3(0f, 0f, -1f));
+        }
+        if (Input.GetKey(KeyCode.Space)) {
+            boxRigid.AddForce(new Vector3(0f, 1f, 0f));
+        }
+        if (Input.GetKey(KeyCode.E)) {
+            if (remainingFiringRate <= 0) {
+                GameObject bulletObj = Instantiate(bullet, boxRigid.transform.position, boxRigid.transform.rotation);
+                bullet.transform.forward = boxRigid.transform.forward;
+                remainingFiringRate = firingRate;
+            }
+            remainingFiringRate -= Time.deltaTime;
+        }
+    }
+}
