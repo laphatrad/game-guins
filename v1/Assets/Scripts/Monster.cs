a0f2b9ff50266d950fa7d03d061a0bf1a55a2f5c
iamtitoo <tsupapol@cmkl.ac.th>
2021-11-20 15:43:28 +0700

    make monster can hit

--- v1/Assets/Scripts/Monster.cs
+++ v1/Assets/Scripts/Monster.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class Monster : MonoBehaviour
 {
     public double hp = 20;
+    public Weapon weapon;
+    public GameObject bullet;
+
+    private double firingRate = 2;
+    private double remainingFiringRate = 0;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        var wpn = weapon.GetComponent<Weapon>();
+        transform.LookAt(wpn.transform);
+
         if (hp <= 0) {
             Die();
         }
+
+        if (remainingFiringRate <= 0) {
+            GameObject bulletObj = Instantiate(bullet, transform.position, transform.rotation);
+            bullet.transform.forward = transform.forward;
+            remainingFiringRate = firingRate;
+        }
+        remainingFiringRate -= Time.deltaTime;
     }
 
     public void TakeDamage(double damage) {
--- v1/Assets/Scripts/Weapon.cs
+++ v1/Assets/Scripts/Weapon.cs
@@ -6,39 +6,60 @@ public class Weapon : MonoBehaviour
 {
     public GameObject bullet;
     public Rigidbody boxRigid;
+    public Transform gunEnd;
+    public double hp = 100;
+
+    public TMPro.TextMeshProUGUI txtHp;
+
     private double firingRate = 0.25;
     private double remainingFiringRate = 0;
+    private double damage = 5;
+    private LineRenderer laserLine;
+    private Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        laserLine = GetComponent<LineRenderer>();
+        camera = GetComponentInParent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A)) {
-            boxRigid.AddForce(new Vector3(-3f, 0f, 0f));
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            boxRigid.AddForce(new Vector3(3f, 0f, 0f));
-        }
-        if (Input.GetKey(KeyCode.W)) {
-            boxRigid.AddForce(new Vector3(0f, 0f, 3f));
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            boxRigid.AddForce(new Vector3(0f, 0f, -3f));
-        }
-        if (Input.GetKey(KeyCode.Space)) {
-            boxRigid.AddForce(new Vector3(0f, 3f, 0f));
-        }
-        if (Input.GetKey(KeyCode.E)) {
-            if (remainingFiringRate <= 0) {
-                GameObject bulletObj = Instantiate(bullet, boxRigid.transform.position, boxRigid.transform.rotation);
-                bullet.transform.forward = boxRigid.transform.forward;
-                remainingFiringRate = firingRate;
+        txtHp.text = hp.ToString();
+        remainingFiringRate -= Time.deltaTime;
+    }
+
+    public void Hit() {
+        if (remainingFiringRate <= 0) {
+            Vector3 lineOrigin = camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+            Debug.DrawRay(lineOrigin, camera.transform.forward * 100f, Color.green);
+            RaycastHit hit;
+            laserLine.SetPosition(0, gunEnd.position);
+            StartCoroutine(ShotEffect());
+            if (Physics.Raycast(lineOrigin, camera.transform.forward, out hit, 100f)) {
+                laserLine.SetPosition (1, hit.point);
+                Monster monster = hit.collider.GetComponent<Monster>();
+
+                if (monster != null) {
+                    monster.TakeDamage(damage);
+                }
             }
-            remainingFiringRate -= Time.deltaTime;
+            else {
+                laserLine.SetPosition (1, lineOrigin + (camera.transform.forward * 100f));
+            }
+            remainingFiringRate = firingRate;
         }
+    }
+
+    public void TakeDamage(double damage) {
+        hp -= damage;
+    }
+
+    private IEnumerator ShotEffect()
+    {
+        laserLine.enabled = true;
+        yield return new WaitForSeconds(0.07f);
+        laserLine.enabled = false;
     }
 }
